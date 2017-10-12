4181b6062f72c36caaf6a259680d59d4d29d6db8
Marc Alexandre <github@malexandre.fr>
2017-10-12 17:30:40 +0200

    Update template to fit eslint config & show off toolbox

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,45 +1,45 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Button } from 'react-toolbox/lib/button'
+import logo from './logo.svg'
+import css from './App.scss'
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <div className="App">
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <h1 className="App-title">Welcome to Malexandres's React Config</h1>
+                <div className={ css.app }>
+                    <header className={ css.header }>
+                        <img src={ logo } className={ css.logo } alt="logo" />
+                        <h1 className={ css.title }>Welcome to Malexandre&apos;s React Config</h1>
                     </header>
-                    <p className="App-intro">React-router and React-toolbox are available!</p>
-                    <Route
-                        exact
+                    <p className={ css.intro }>React-router and React-toolbox are available!</p>
+                    <Route exact
                         path="/"
-                        render={() => (
+                        render={ () => (
                             <div>
-                                <p>This is the route "/"!</p>
+                                <p>This is the route &quot;/&quot;!</p>
                                 <p>
-                                    <Link to="/about">Change route to "/about"</Link>
+                                    <Link to="/about">Change route to &quot;/about&quot;</Link>
+                                </p>
+                                <p>
+                                    <Button icon="bookmark" label="Bookmark" raised primary />
                                 </p>
                             </div>
-                        )}
-                    />
-                    <Route
-                        path="/about"
-                        render={() => (
+                        ) }/>
+                    <Route path="/about"
+                        render={ () => (
                             <div>
-                                <p>This is the route "/about"!</p>
+                                <p>This is the route &quot;/about&quot;!</p>
                                 <p>
-                                    <Link to="/">Change route to "/"</Link>
+                                    <Link to="/">Change route to &quot;/&quot;</Link>
                                 </p>
                             </div>
-                        )}
-                    />
+                        ) }/>
                 </div>
             </Router>
-        );
+        )
     }
 }
 
-export default App;
+export default App
